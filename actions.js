b9fd0e2d738559871f56c7d5a540fdd51a29fa24
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-29 11:19:22 -0500

    project post working

--- actions.js
+++ actions.js
@@ -3,6 +3,19 @@ const router = express.Router();
 const ActionDB = require('./data/helpers/actionModel.js'); 
 
 
+router.get('/', async (req, res) => {
+    try {
+        const action = await ActionDB.get(req.query); 
+        res.status(200)
+        .json(action)
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Can't get actions. Error: ${error}`
+        })
+    }
+})
+
 router.get('/:id', async (req, res) => {
     try {
         const action = await ActionDB.get(req.params.id);
@@ -22,6 +35,61 @@ router.get('/:id', async (req, res) => {
         })
     }
 })
+
+router.post('/', async (req, res) => {
+    try {
+        const postAction = await ActionDB.insert(req.body); 
+        res.status(201)
+        .json(postAction)
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Cannot create action. Error: ${error}`
+        })
+    }
+})
+
+
+router.put('/:id', async (req, res) => {
+    try {
+        const putAction = await ActionDB.update(req.params.id, req.body); 
+        if (putAction) {
+            res.status(200)
+            .json(putAction)
+        } else {
+            res.status(404)
+            .json({
+                message: `Cannot find action to update.`
+            })
+        }
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Cannot update action. Error: ${error}`
+        })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteAction = await ActionDB.remove(req.params.id); 
+        if (deleteAction) {
+            res.status(200)
+            .json(deleteAction)
+        } else {
+            res.status(404)
+            .json({
+                message: `Cannot find action to delete`
+            })
+        }
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Cannot delete action. Error: ${error}`
+        })
+    }
+})
+
 
 
 
--- projects.js
+++ projects.js
@@ -22,6 +22,62 @@ router.get('/:id', async (req, res) => {
 })
 
 
+router.post('/', async (req, res) => {
+    try {
+        const postProject = await ProjectDB.insert(req.body); 
+        res.status(201)
+        .json(postProject)
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Cannot create action. Error: ${error}`
+        })
+    }
+})
+
+
+router.put('/:id', async (req, res) => {
+    try {
+        const putProject = await ProjectDB.update(req.params.id, req.body); 
+        if (putProject) {
+            res.status(200)
+            .json(putProject)
+        } else {
+            res.status(404)
+            .json({
+                message: `Cannot find action to update.`
+            })
+        }
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Cannot update action. Error: ${error}`
+        })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteProject = await ProjectDB.remove(req.params.id); 
+        if (deleteProject) {
+            res.status(200)
+            .json(deleteProject)
+        } else {
+            res.status(404)
+            .json({
+                message: `Cannot find action to delete`
+            })
+        }
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Cannot delete action. Error: ${error}`
+        })
+    }
+})
+
+
+
 
 
 
